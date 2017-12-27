dcbc39b94eb68fe80542e672dda7cce8659f5208
ArvidSteen <arvid.agaton.steen@gmail.com>
2017-12-27 16:46:54 +0100

    complete change

---
+++ nexus2/header.php
@@ -0,0 +1,13 @@
+<header id="mainLinkbar">
+	<div class="wrapper">
+		<img src="logo/logo.jpg">
+		<nav role="topNav">
+			<ul>
+				<li><a href="index.htm">HOME</a></li>
+				<li><a href="galery.htm">GALERY</a></li>
+				<li><a href="forum.htm">FORUM</a></li>
+				<li><a href="contact.htm">CONTACT</a></li>
+			</ul>
+		</nav>
+	</div>
+</header>
